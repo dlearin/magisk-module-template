fc1baad187f0b7d55665a93439067f4c6c26216a
Dlearin <cfernande1408@gmail.com>
2023-10-19 13:14:22 +0200

    added libmegface patch

--- config.sh
+++ config.sh
@@ -10,7 +10,7 @@
 # 
 # Instructions:
 # 
-# 1. Place your files into system folder (delete the placeholder file)
+# 1. Place your files into the system folder (delete the placeholder file)
 # 2. Fill in your module's info into module.prop
 # 3. Configure the settings in this file (common/config.sh)
 # 4. For advanced features, add shell commands into the script files under common:
@@ -50,7 +50,7 @@ LATESTARTSERVICE=false
 
 print_modname() {
   ui_print "*******************************"
-  ui_print "     Magisk Module Template    "
+  ui_print "     Fix for face Unlock Redwood ArrowOS    "
   ui_print "*******************************"
 }
 
@@ -59,20 +59,9 @@ print_modname() {
 ##########################################################################################
 
 # List all directories you want to directly replace in the system
-# By default Magisk will merge your files with the original system
+# By default, Magisk will merge your files with the original system
 # Directories listed here however, will be directly mounted to the correspond directory in the system
 
-# You don't need to remove the example below, these values will be overwritten by your own list
-# This is an example
-REPLACE="
-/system/app/Youtube
-/system/priv-app/SystemUI
-/system/priv-app/Settings
-/system/framework
-"
-
-# Construct your own list here, it will overwrite the example
-# !DO NOT! remove this if you don't need to replace anything, leave it empty as it is now
 REPLACE="
 "
 
@@ -84,18 +73,8 @@ REPLACE="
 
 set_permissions() {
   # Default permissions, don't remove them
-  set_perm_recursive  $MODPATH  0  0  0755  0644
+  set_perm_recursive $MODPATH 0 0 0755 0644
 
-  # Only some special files require specific permissions
-  # The default permissions should be good enough for most cases
-
-  # Some templates if you have no idea what to do:
-
-  # set_perm_recursive  <dirname>                <owner> <group> <dirpermission> <filepermission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm_recursive  $MODPATH/system/lib       0       0       0755            0644
-
-  # set_perm  <filename>                         <owner> <group> <permission> <contexts> (default: u:object_r:system_file:s0)
-  # set_perm  $MODPATH/system/bin/app_process32   0       2000    0755         u:object_r:zygote_exec:s0
-  # set_perm  $MODPATH/system/bin/dex2oat         0       2000    0755         u:object_r:dex2oat_exec:s0
-  # set_perm  $MODPATH/system/lib/libart.so       0       0       0644
+  # Set the permission for libmegface.so
+  set_perm $MODPATH/system/lib64/libmegface.so 0 0 0644
 }
